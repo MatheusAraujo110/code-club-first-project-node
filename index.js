769178278b7dfa2166de415854c15864fc94bc6d
MatheusAraujo110 <theusnascimento386@gmail.com>
2024-06-10 20:20:07 -0300

    reposiótio criado

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const uuid = require('uuid')
+import express from 'express';
+import { v4 } from 'uuid';
+import cors from "cors"
 
-const port = 3000
-const app = express()
-app.use(express.json())
+const port = 3001;
+const app = express();
+app.use(express.json());
+app.use(cors())
 
 // - Query params => meusite.com/users?nome=matheus&age=23    //FILTROS
 // - Route params => /users/2     //BUSCAR, DELETAR OU ATUALIZAR ALGO ESPECÍFICO
@@ -17,52 +19,36 @@ app.use(express.json())
 
 // - Middlewarre => INTERCEPTADOR => Tem o pooder de parar ou alterar dados da requisição.
 
-const users = []
+const users = [];
 
-const checkUserId = (request, response, next) => {
-    const { id } = request.params
+const checkUserID = (request, response, next) => {
+    const { id } = request.params;
 
-    const index = users.findIndex(user => user.id === id)
+    const index = users.findIndex((user) => user.id === id);
 
     if (index < 0) {
-        return response.status(404).json({ error: "user not found" })
+        return response.status(404).json({ messager: "user not found" });
     }
 
-    request.userIndex = index
-    request.userId = id
-
-    next()
+    request.userIndex = index;
+    request.userId = id;
+    next();
 }
 app.get('/users/', (request, response) => {
-
-    return response.json(users)
+    return response.json(users);
 });
 
-/*
-TRATAMENTOS DE ERROS (TRY CATCH)
-*/
 
 app.post('/users/', (request, response) => {
-    try {
+    const { name, age } = request.body;
+    const user = { id: v4(), name, age };
 
-        const { name, age } = request.body
+    users.push(user);
 
-        if (age < 18) throw new Error("Only allowed users over 18 years old")
-
-        const user = { id: uuid.v4(), name, age }
-
-        users.push(user)
-
-        return response.status(201).json(user)
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
-    finally {
-        console.log("terminou tudo")
-    }
+    return response.status(201).json(user);
 });
 
-app.put('/users/:id', checkUserId, (request, response) => {
+app.put('/users/:id', checkUserID, (request, response) => {
     const { name, age } = request.body
     const index = request.userIndex
     const id = request.userId
@@ -75,12 +61,12 @@ app.put('/users/:id', checkUserId, (request, response) => {
 });
 
 
-app.delete('/users/:id', checkUserId, (request, response) => {
-    const index = request.userIndex
+app.delete('/users/:id', checkUserID, (request, response) => {
+    const index = request.userIndex;
 
-    users.splice(index, 1)
+    users.splice(index, 1);
 
-    return response.status(204).json()
+    return response.status(204).json();
 });
 
 
